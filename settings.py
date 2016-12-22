5589a7af3bddee1f3e93b4c4f2f667c01b45d555
cucole <lzgcucool@gmail.com>
2016-12-22 14:15:56 +0800

    del some code

--- settings.py
+++ settings.py
@@ -132,29 +132,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/').replace('\\','/')
 STATICFILES_DIRS = (
   os.path.join(SITE_ROOT, 'static/'),
 )
-
-LOGGING = {  
-    'disable_existing_loggers': False,  
-    'version': 1,  
-    'handlers': {  
-        'console': {  
-            # logging handler that outputs log messages to terminal  
-            'class': 'logging.StreamHandler',  
-            'level': 'DEBUG', # message level to be written to console  
-        },  
-    },  
-    'loggers': {  
-        '': {  
-            # this sets root level logger to log debug and higher level  
-            # logs to console. All other loggers inherit settings from  
-            # root level logger.  
-            'handlers': ['console'],  
-            'level': 'DEBUG',  
-            'propagate': False, # this tells logger to send logging message  
-                                # to its parent (will send if set to True)  
-        },  
-        'django.db': {  
-            # django also has database level logging  
-        },  
-    },  
-}  
